e513eeb56ad184fa581e15e9d81e1ca67af69eab
wmorota <wesleyraemorota@gmail.com>
2020-04-27 20:11:02 -0400

    Created date-and-time.js

---
+++ date-and-time.js
@@ -0,0 +1,7 @@
+// grab current.dt from JSON of OneCall
+function getDateTime(timestamp) {
+  console.log(new Date(timestamp));
+  console.log(timestamp);
+
+  //+ "Updated as of: " + timestamp ;
+}
