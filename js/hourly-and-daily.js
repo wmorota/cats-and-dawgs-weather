9d6c115421258f7994fdbab932487fcaea68e158
wmorota <wesleyraemorota@gmail.com>
2020-04-29 02:42:07 -0400

    Added Hourly and Daily buttons that properly change colors

---
+++ js/hourly-and-daily.js
@@ -0,0 +1,21 @@
+function clickDaily()
+{
+  document.getElementById('daily').style.backgroundColor = "#96CCFF";
+  document.getElementById('daily').style.borderColor = "#96CCFF";
+  document.getElementById('daily').style.color = "white";
+
+  document.getElementById('hourly').style.backgroundColor = "white";
+  document.getElementById('hourly').style.borderColor = "#96CCFF";
+  document.getElementById('hourly').style.color = "#96CCFF";
+}
+
+function clickHourly()
+{
+  document.getElementById('hourly').style.backgroundColor = "#96CCFF";
+  document.getElementById('hourly').style.borderColor = "#96CCFF";
+  document.getElementById('hourly').style.color = "white";
+
+  document.getElementById('daily').style.backgroundColor = "white";
+  document.getElementById('daily').style.borderColor = "#96CCFF";
+  document.getElementById('daily').style.color = "#96CCFF";
+}
