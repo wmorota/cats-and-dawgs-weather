433240419f41efb9705273adfdba5cc57c3bbcae
wmorota <wesleyraemorota@gmail.com>
2020-04-29 19:12:10 -0400

    Can now output searched cities along with their weather data

--- js/search-location.js
+++ js/search-location.js
@@ -1,5 +1,12 @@
-var city_array = [];
 var city_temps = [];
+var city_names = [];
+var city_countries = [];
+var city_weathers = [];
+
+var c_name;
+var c_country;
+var c_temp;
+var c_weather;
 
 function searchCity(){
   var input = document.getElementById("search-city").value;
@@ -21,13 +28,60 @@ function searchCity(){
         alert("City not found 😿 Please try again");
       }
       else{
-        console.log("it works");
+
+        c_name = data.name;
+        c_country = data.sys.country;
+        c_temp = Math.round(data.main.temp) + "°C";
+        c_weather = data.weather[0].main;
+
+        console.log("ok it works");
+        console.log(c_name);
+        console.log(c_country);
+        console.log(c_temp);
+        console.log(c_weather);
+
+        // if weather gets 'Clouds' then it just turns it to 'Cloudy' LOL
+        if(c_weather == "Clouds"){
+          c_weather = "Cloudy";
+        }
+
+//if(city_name.length <= 4)
+        city_names.push(c_name);
+        city_countries.push(c_country);
+        city_temps.push(c_temp);
+        city_weathers.push(c_weather);
+
+        console.log(city_names);
+        console.log(city_countries);
+        console.log(city_temps);
+        console.log(city_weathers);
+
+        showCity();
+
+        // push into respective arrays
+        // check if city already exists
+        // some x that takes off that div-city[i] then replacing it or something
       }
 
     })
     .catch(err => {
 
     })
+}
 
+function showCity() {
+  for(var i = 0; i < city_names.length; i++){
+    document.getElementById('city-div' + i).style.backgroundColor = "#96CCFF";
+    document.getElementById('city-div' + i).style.color = "white";
+    document.getElementById('div'+ i + "-name").innerHTML = city_names[i];
+    document.getElementById('div'+ i + "-country").innerHTML = city_countries[i];
+    document.getElementById('div'+ i + "-country").style.color = "white";
+    document.getElementById('div'+ i + "-country").style.fontSize = "15px";
+    document.getElementById('div'+ i + "-temp").innerHTML = city_temps[i];
+    document.getElementById('div'+ i + "-temp").style.color = "white";
+    document.getElementById('div'+ i + "-weather").innerHTML = city_weathers[i];
+    document.getElementById('div'+ i + "-weather").style.color = "white";
+    document.getElementById('div'+ i + "-weather").style.fontSize = "15px";
+  }
 
 }
