86916fea173c33c8f3f343d28be6a1209a7e11cb
wmorota <wesleyraemorota@gmail.com>
2020-04-29 21:40:46 -0400

    Can now refresh cities' weather data as well + made input as title case

--- js/search-location.js
+++ js/search-location.js
@@ -9,6 +9,7 @@ var c_temp;
 var c_weather;
 
 function searchCity(){
+
   var input = document.getElementById("search-city").value;
   console.log(input);
 
@@ -50,6 +51,10 @@ function searchCity(){
           city_temps.push(c_temp);
           city_weathers.push(c_weather);
 
+          if(city_names.length > 4){
+            alert('Saving, deleting, and adding of more than 4 cities will be available in Version 2.0 of Cats and Dawgs 🐱🐶');
+          }
+
 
         console.log(city_names);
         console.log(city_countries);
@@ -57,10 +62,7 @@ function searchCity(){
         console.log(city_weathers);
 
         showCity();
-
-        // push into respective arrays
-        // check if city already exists
-        // some x that takes off that div-city[i] then replacing it or something
+        document.getElementById('searchForm').reset();
       }
 
     })
@@ -84,7 +86,6 @@ function showCity() {
       document.getElementById('city-div' + i).style.backgroundColor = "#7DA6CC";
     }
 
-
     document.getElementById('city-div' + i).style.color = "white";
     document.getElementById('div'+ i + "-name").innerHTML = city_names[i];
     document.getElementById('div'+ i + "-country").innerHTML = city_countries[i];
@@ -96,5 +97,74 @@ function showCity() {
     document.getElementById('div'+ i + "-weather").style.color = "white";
     document.getElementById('div'+ i + "-weather").style.fontSize = "15px";
   }
+}
 
+function refreshCity() {
+  var refreshCity0_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city_names[0] + "&units=metric&appid=bd007df1fd5fd56285b9b4e42f0b658b";
+
+  // FOR ZERO
+  fetch(refreshCity0_URL).then(response => {
+      return response.json()
+    })
+    .then(data => {
+        c_temp = Math.round(data.main.temp) + "°C";
+        c_weather = data.weather[0].main;
+
+        console.log("zero");
+        console.log("refreshed zero temp: " + c_temp);
+        console.log("refreshed zero weather: " + c_weather);
+
+        // if weather gets 'Clouds' then it just turns it to 'Cloudy' LOL
+        if(c_weather == "Clouds"){
+          c_weather = "Cloudy";
+        }
+
+          city_temps[0] = c_temp;
+          city_weathers[0] = c_weather;
+
+          console.log(city_temps[0] + " is now: " + c_temp);
+          console.log(city_weather[0] + " is now: " + c_weather);
+
+          console.log('iteration: ' + i);
+
+        showCity();
+    })
+    .catch(err => {
+    })
+
+
+
+  for(var i = 1; i < city_names.length ; i++){
+    var refreshCity_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city_names[i] + "&units=metric&appid=bd007df1fd5fd56285b9b4e42f0b658b";
+
+    // How to fetch online JSON files from a given URL
+    fetch(refreshCity_URL).then(response => {
+        return response.json()
+      })
+      .then(data => {
+          c_temp = Math.round(data.main.temp) + "°C";
+          c_weather = data.weather[0].main;
+
+          console.log("refreshed");
+          console.log("refreshed temp: " + c_temp);
+          console.log("refreshed weather: " + c_weather);
+
+          // if weather gets 'Clouds' then it just turns it to 'Cloudy' LOL
+          if(c_weather == "Clouds"){
+            c_weather = "Cloudy";
+          }
+
+            city_temps[i-1] = c_temp;
+            city_weathers[i-1] = c_weather;
+
+            console.log(city_temps[i - 1] + " is now: " + c_temp);
+            console.log(city_weather[i - 1] + " is now: " + c_weather);
+
+            console.log('iteration: ' + i);
+
+          showCity();
+      })
+      .catch(err => {
+      })
+  }
 }
